fba1b511262816084251d2406cb979d795d1e3ba
Yousuf <firedragonironfist998@gmail.com>
2022-11-09 20:06:14 +0530

    learning js and react

---
+++ React Course/js refresher/day_1.js
@@ -0,0 +1,35 @@
+// in-line comment
+/* this is a multi line comment */
+//Data types: undefined, null, boolean, string, symbol, number and object
+
+var myName = "Beau";
+
+myName =8;
+
+let ourName="freeCodeCamp";
+
+const pi = 3.14;
+
+var a;
+var b = 2;
+
+console.log(a);
+a = 7;
+
+b = a;
+
+//allows to see things in the console
+console.log(a);
+
+var a = 5;
+var b = 10;
+var c = "I am a ";
+
+a = a +1;
+b = b +5;
+c = c + "String!";
+
+//capatilization matters so, 
+var StUdLyCapVaR;
+// is not equal to
+var studlycapvar;
