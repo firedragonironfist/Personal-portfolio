fc060bba8571d0999faf4ff14e241e7fb2ca1d67
Yousuf <firedragonironfist998@gmail.com>
2022-11-23 18:59:14 +0530

    React added

--- React Course/js refresher/day_1.js
+++ React Course/js refresher/day_1.js
@@ -2,7 +2,7 @@
 /* this is a multi line comment */
 //Data types: undefined, null, boolean, string, symbol, number and object
 
-var myName = "Beau";
+/*var myName = "Beau";
 
 myName =8;
 
@@ -33,3 +33,38 @@ c = c + "String!";
 var StUdLyCapVaR;
 // is not equal to
 var studlycapvar;
+
+// + ->adding
+// - ->subtracting
+// * ->multiplying
+// / ->dividing
+
+var myVar = 87;
+myVar ++; //incrementing
+myVar --; //decrementing
+*/
+
+// single quote can be written to take double qutoes as output 
+/* var myStr ='Hello my "name" is yousuf';
+console.log(myStr); */
+
+//escape sequence
+// \' single quote
+// \" single quote
+// \\ single quote
+// \n single quote
+// \r single quote
+// \t single quote
+// \b single quote
+// \f single quote 
+
+var firstLetterOfFirstName;
+var firstNameLength =0;
+var firstName = "Ada";
+firstNameLength=firstName.length;
+firstLetterOfFirstName = firstName[0];
+console.log(firstNameLength);
+console.log(firstLetterOfFirstName);
+
+//it has zero based indexing of the string
+//strings are immutable
